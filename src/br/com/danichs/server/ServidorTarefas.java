07425223f567bec6a88dd9e922a96240d6e38f78
Joao Victor Cardozo <joao.cardozo@asaas.com.br>
2024-05-06 15:53:14 -0300

    Adição de thread pool para crescimento dinamico de threads

--- src/br/com/danichs/server/ServidorTarefas.java
+++ src/br/com/danichs/server/ServidorTarefas.java
@@ -2,6 +2,8 @@ package br.com.danichs.server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefas {
     public static void main(String[] args) throws Exception {
@@ -9,14 +11,16 @@ public class ServidorTarefas {
         
         System.out.println("--- Iniciando servidor---");
         ServerSocket servidor = new ServerSocket(12345);
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
 
         while (true) {
             Socket socket = servidor.accept();
             System.out.println("aceitando novo cliente na porta : " + socket.getPort());
 
             DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-            Thread threadCliente = new Thread(distribuirTarefas);
-            threadCliente.start();
+            threadPool.execute(distribuirTarefas);
+        
         }
     }
 }
